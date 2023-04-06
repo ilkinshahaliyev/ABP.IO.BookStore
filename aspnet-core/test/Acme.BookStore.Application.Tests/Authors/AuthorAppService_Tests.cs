7d12879f1882e4de39ca85460d13deb5d54aead0
Ilkin Shahaliyev <i.shahaliyev@tayqatech.com>
2023-04-06 10:34:01 +0400

    The tests of ApplicationService of authors added

---
+++ aspnet-core/test/Acme.BookStore.Application.Tests/Authors/AuthorAppService_Tests.cs
@@ -0,0 +1,68 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acme.BookStore.Authors
+{
+    public class AuthorAppService_Tests : BookStoreApplicationTestBase
+    {
+        private readonly IAuthorAppService _authorAppService;
+
+        public AuthorAppService_Tests()
+        {
+            _authorAppService = GetRequiredService<IAuthorAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_All_Authors_Without_Any_Filter()
+        {
+            var result = await _authorAppService.GetListAsync(new());
+
+            result.TotalCount.ShouldBeGreaterThanOrEqualTo(2);
+            result.Items.ShouldContain(author => author.Name == "George Orwell");
+            result.Items.ShouldContain(author => author.Name == "Douglas Adams");
+        }
+
+        [Fact]
+        public async Task Should_Get_Filtered_Authors()
+        {
+            var result = await _authorAppService.GetListAsync(new()
+            {
+                Filter = "George"
+            });
+
+            result.TotalCount.ShouldBeGreaterThanOrEqualTo(1);
+            result.Items.ShouldContain(author => author.Name == "George Orwell");
+            result.Items.ShouldNotContain(author => author.Name == "Douglas Adams");
+        }
+
+        [Fact]
+        public async Task Should_Create_A_New_Author()
+        {
+            var authorDto = await _authorAppService.CreateAuthor(new()
+            {
+                Name = "Edward Bellamy",
+                BirthDate = new(1850, 5, 22),
+                ShortBio = "Edward Bellamy was an American author..."
+            });
+
+            authorDto.Id.ShouldNotBe(Guid.Empty);
+            authorDto.Name.ShouldBe("Edward Bellamy");
+        }
+
+        [Fact]
+        public async Task Should_Not_Allow_To_Create_Duplicate_Author()
+        {
+            await Assert.ThrowsAsync<AuthorAlreadyExistsException>(async () =>
+            {
+                await _authorAppService.CreateAuthor(new()
+                {
+                    Name = "Douglas Adams",
+                    BirthDate = DateTime.Now,
+                    ShortBio = "Amazing author..."
+                });
+            });
+        }
+    }
+}
